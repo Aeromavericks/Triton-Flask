882181c603609fc2f5eb2b7f6fe82e6147407726
Nihal Kumarswamy <kumarnihal04@gmail.com>
2022-05-18 15:56:06 -0500

    testing values

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', value = '100')
 
 
 if __name__ == '__main__':
