9e478fec71f516bc9c56453ca51aa724762261b9
Nihal Kumarswamy <kumarnihal04@gmail.com>
2022-05-19 15:13:58 -0500

    testing to turn on valve1 PB9

--- serial_controller.py
+++ serial_controller.py
@@ -54,7 +54,7 @@ class Controller():
         if valve == 'valve1':
             self.ser.write('A'.encode())
             print('Sent A')
-            print(ser.readline())
+            print(self.ser.readline())
         elif valve == 'valve2':
             self.ser.write('B'.encode())
         elif valve == 'valve3':  
