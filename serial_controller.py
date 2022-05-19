a5a0f864e05543c58c43bd22f1169c06ac7cb05f
Nihal Kumarswamy <kumarnihal04@gmail.com>
2022-05-19 14:44:21 -0500

    testing to turn on valve1 PB9

--- serial_controller.py
+++ serial_controller.py
@@ -32,7 +32,7 @@ class Controller():
             tmp = serial.Serial(x.device)
             tmp.reset_input_buffer()
             mode = ''
-            while mode != 'V' or mode !='P':
+            while len(mode) != 1:
                 mode = tmp.readline().decode().split(',')[0]
                 print(mode)
             if mode == self.typeof_mc:
