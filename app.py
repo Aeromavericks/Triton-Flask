a57f2f16f37344d93d4c7e94f20555ff90ba990a
itf140030 <37088881+itf140030@users.noreply.github.com>
2022-05-23 12:01:16 -0500

    Real time plot
    Real time plot injected with random data through python.
    
    added libraries to enable real time plotting

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, stream_with_context, Response
 import serial_controller
 from turbo_flask import Turbo
-import threading, time, sys, random,json
+import threading, time, sys, random, webbrowser,json,logging
+from datetime import datetime
+from typing import Iterator
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 turbo = Turbo(app)
+random.seed()
 #pressure_controller = serial_controller.Controller('pressure')
 
 #valve_controller = serial_controller.Controller('valve')
 values = [600]
 @app.route('/')
 def index():
-    legend = 'Random Num'
-    return render_template('index.html',legend=legend)
+    return render_template('index.html')
+
+def generate_random_data() -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    if request.headers.getlist("X-Forwarded-For"):
+        client_ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        client_ip = request.remote_addr or ""
+
+    try:
+        logger.info("Client %s connected", client_ip)
+        while True:
+            json_data = json.dumps(
+                {
+                    "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    "value": random.randrange(0,1000,1),
+                }
+            )
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+    except GeneratorExit:
+        logger.info("Client %s disconnected", client_ip)
 
 
 #@app.route('/valve_toggle/<valvename>')
@@ -20,6 +49,13 @@ def index():
     #call change valve here
     #valve_controller.change_valve(valvename)
     #return {valvename:'changed'}
+
+@app.route("/chart-data")
+def chart_data() -> Response:
+    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
 
 @app.before_first_request
 def before_first_request():
@@ -60,5 +96,5 @@ if __name__ == '__main__':
 #    pressure_controller.connect()
 #    valve_controller.connect()
     
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True,threaded=True)
     
