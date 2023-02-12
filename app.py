818b24a0887aedcfd2bd1a5011a32551de6bda75
Nihal Kumarswamy <kumarnihal04@gmail.com>
2023-02-12 01:08:32 -0600

    app.py file clean up and deleted unused template files

--- app.py
+++ app.py
@@ -1,46 +1,38 @@
 from flask import Flask, render_template, request, stream_with_context, Response
 import serial_controller
-import threading, time, sys, random, webbrowser,json,logging
+import threading, time,json
 from datetime import datetime
 from typing import Iterator
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
-logger = logging.getLogger(__name__)
-
-random.seed()
 pressure_controller = serial_controller.Controller('pressure')
 
 valve_controller = serial_controller.Controller('valve')
-sleeptime = 0.1
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def generate_random_data() -> Iterator[str]:
+def pressure_data_source() -> Iterator[str]:
     while True:
         pressures = pressure_controller.get_p()
-        json_data_chart = json.dumps(
-            {
-                "time": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                "value1": int(pressures[1]),
-                "value2": int(pressures[2]),
-            }
-            )
-        j=j+1
+
+        json_data_chart = json.dumps({
+            "time": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "value1": int(pressures[1]),
+            "value2": int(pressures[2]),
+            })
+
         yield f"data:{json_data_chart}\n\n"
             
 
 @app.route('/valve_toggle/<valvename>')
 def valve_toggle(valvename):
-    #call change valve here
     valve_controller.change_valve(valvename)
     return {valvename:'changed'}
 
 @app.route("/data")
 def chart_data() -> Response:
-    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    response = Response(stream_with_context(pressure_data_source()), mimetype="text/event-stream")
     response.headers["Cache-Control"] = "no-cache"
     response.headers["X-Accel-Buffering"] = "no"
     return response
