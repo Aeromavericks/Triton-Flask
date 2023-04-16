39f96bab7179429cde3a484e6be4f30e27ad7e24
Nihal Kumarswamy <kumarnihal04@gmail.com>
2023-04-16 10:58:33 -0500

    added multithreading

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template
 import serial_controller
 import serial_controllerStub
 import sys
+from threading import Thread, Lock
 
 app = Flask(__name__) 
 
 pressure_controller = None
 valve_controller = None
+last_pressure = {"P1": 0, "P2": 0}
+mutex = Lock()
 
 def initializeSerialControllers() -> None:
     global pressure_controller, valve_controller
@@ -29,9 +32,23 @@ def valve_toggle(valvename):
 
 @app.route("/pressure_data")
 def pressure_data():
-    pressures = pressure_controller.get_p()
-    return {"P1": pressures[1], "P2": pressures[2]}
+    global last_pressure
+    mutex.acquire()
+    tmp = last_pressure
+    mutex.release()
+    return tmp
 
+def worker_thread():
+    global last_pressure
+    while True:
+        pressures = pressure_controller.get_p()
+        tmp = {"P1": pressures[1], "P2": pressures[2], "time": time.time()}
+        #write db part
+        print(tmp)
+        mutex.acquire()
+        last_pressure = tmp
+        mutex.release()
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     
@@ -42,4 +59,6 @@ if __name__ == '__main__':
 
     pressure_controller.connect()
     valve_controller.connect()
+    t = Thread(target = worker_thread)
+    t.run()
     app.run(host='0.0.0.0', threaded=True)
